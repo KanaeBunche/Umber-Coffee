5cbf79aa44da9dba186318933bb64532dae7e94b
Kanae <kanae.bunche@gmail.com>
2026-07-05 06:00:48 -0400

    push

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ import Staff from './pages/Staff'
 import Menu from './pages/Menu'
 import About from './pages/About'
 import Account from './pages/Account'
-import FloatingCTA from './components/Floatingcta'
+import FloatingCTA from './components/FloatingCTA'
 import './index.css'
 
 function Home({ onOrderClick }) {
